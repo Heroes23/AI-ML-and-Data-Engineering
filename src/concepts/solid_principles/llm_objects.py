80847a2488bb98fa1e9e4b1356be7129faba0151
Zaed Hussain <86385393+Heroes23@users.noreply.github.com>
2026-04-28 11:13:43 -0500

    Delete src/concepts directory

--- src/concepts/solid_principles/llm_objects.py
+++
@@ -1,9 +0,0 @@
-# Class
-class ChatGPT:
-    # Constructor
-    def __init__(self, api_key: str, model_name: str):
-        self.api = api_key
-        self.model = model_name
-    
-    def chat(self, prompt: str):
-        print(f"Undergoing instructions from human: {prompt}")
